b6f65511d5f029ee608227354a05e542e09d0521
Thomas Leese <thomas@tlsquared.co.uk>
2026-04-17 14:59:11 +0100

    Add instance access and secret keys

--- main.tf
+++ main.tf
@@ -50,6 +50,27 @@ resource "scaleway_instance_security_group" "main" {
   }
 }
 
+resource "scaleway_iam_application" "main_instance" {
+  name = "forge-main-instance"
+}
+
+resource "scaleway_iam_policy" "main_instance_secret_manager_read_only" {
+  name           = "forge-main-instance-secret-manager-read-only"
+  application_id = scaleway_iam_application.main_instance.id
+
+  rule {
+    project_ids = [var.scaleway_project_id]
+
+    permission_set_names = [
+      "SecretManagerReadOnly"
+    ]
+  }
+}
+
+resource "scaleway_iam_api_key" "main_instance" {
+  application_id = scaleway_iam_application.main_instance.id
+}
+
 resource "scaleway_instance_server" "main" {
   name = "forge-main"
 
@@ -68,5 +89,7 @@ resource "scaleway_instance_server" "main" {
 
   user_data = {
     cloud-init = file("${path.module}/cloud-init.yaml")
+    scw-access-key = scaleway_iam_api_key.main_instance.access_key
+    scw-secret-key = scaleway_iam_api_key.main_instance.secret_key
   }
 }
