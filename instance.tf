91fdbe277fb629ad217c0815cd878333ac4c3b5e
Thomas Leese <thomas@tlsquared.co.uk>
2026-04-17 10:07:41 +0100

    Add Scaleway instance configuration

--- instance.tf
+++ instance.tf
@@ -11,3 +11,46 @@ resource "scaleway_block_volume" "data" {
   iops       = 5000
   size_in_gb = 10
 }
+
+resource "scaleway_instance_security_group" "main" {
+  name = "forge-main"
+
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+
+  inbound_rule {
+    action = "accept"
+    port   = "22"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "80"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "443"
+  }
+}
+
+resource "scaleway_instance_server" "main" {
+  name = "forge-main"
+
+  type  = "DEV1-S"
+  image = "debian_trixie"
+
+  ip_ids = [scaleway_instance_ip.public_ipv4.id, scaleway_instance_ip.public_ipv6.id]
+  security_group_id = scaleway_instance_security_group.main.id
+
+  root_volume {
+    name       = "forge-system"
+    size_in_gb = 10
+  }
+
+  additional_volume_ids = [scaleway_block_volume.data.id]
+
+  user_data = {
+    cloud-init = file("${path.module}/cloud-init.yaml")
+  }
+}
